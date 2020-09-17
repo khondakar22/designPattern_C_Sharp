d1295bf945aebdab10e145f3fd11b51b0ae4dc5d
r.islam <r.islam@id-ware.com>
2020-09-17 16:30:31 +0200

    Add- Single and open principle

---
+++ SolidPrinciple/SolidPrinciple/SolidPrinciple/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SolidPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Single Responsibility Principle
+
+            //var journal = new SingleResponsibilityPrinciple();
+            //journal.AddEntry("I cried today");
+            //journal.AddEntry("I ate a bug");
+            //Console.WriteLine(journal);
+
+            //var p = new Persistence();
+            //var filename = @"c:\temp\journal.txt";
+            //p.Save(journal, filename, true);
+            //Process.Start(new ProcessStartInfo(filename) { UseShellExecute = true });
+
+
+            // Open Closed Principle
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Huge);
+
+            Product[] products = { apple, tree, house };
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            Console.WriteLine("Large blue Items:");
+
+            foreach (var p in bf.Filter(products, new AndSpecification<Product>(
+                new ColorSpecification(Color.Blue),
+                new SizeSpecification(Size.Huge)
+            )))
+            {
+                Console.WriteLine($" - {p.Name} is blue and huge");
+            }
+        }
+    }
+}
---
+++ SolidPrinciple/SolidPrinciple/SolidPrinciple/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SolidPrinciple
+{
+    public class SingleResponsibilityPrinciple
+    {
+
+        private readonly List<string> _entries = new List<string>();
+        private static int _count = 0;
+
+        public int AddEntry(string text)
+        {
+            _entries.Add($"{++_count}: {text}");
+            return _count; // memento
+        }
+
+        public void RemoveEntry(int index) => _entries.RemoveAt(index);
+
+        public override string ToString() => string.Join(Environment.NewLine, _entries);
+    }
+
+    public class Persistence
+    {
+        public void Save(SingleResponsibilityPrinciple journal, string filename, bool overwrite = false)
+        {
+            if(overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+
+    }
+}
